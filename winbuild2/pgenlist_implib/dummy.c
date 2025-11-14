c719a127910786e590bd399045ac0b625b13440b
kenjiuno <ku@digitaldolphins.jp>
2025-11-14 22:50:13 +0900

    Adding winbuild2

---
+++ winbuild2/pgenlist_implib/dummy.c
@@ -0,0 +1,5 @@
+void EnlistInDtc() {}
+void DtcOnDisconnect() {}
+void IsolateDtcConn() {}
+__declspec(dllexport) void GetTransactionObject() {}
+__declspec(dllexport) void ReleaseTransactionObject() {}
